93bdf0694c92152822a3cf7e4b2c7b3d5363b48a
AbdulRehman Zia <abdulrehmanzia2021@gmail.com>
2024-11-11 09:41:50 +0500

    started auth

--- src/app/(admin)/admin/layout.js
+++ src/app/(admin)/admin/layout.js
@@ -1,40 +1,50 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Link from "next/link";
 
-function Layout({ children }) {
-    return (<html lang="en">
+import { auth } from "../../../../auth";
+import { redirect } from "next/navigation";
+async function Layout({ children }) {
+  const session = await auth();
+  console.log("Session=>", session);
+
+  // if (!session) redirect("/signin")
+    if (!session) redirect('/signin');
+
+
+  return (
+    <html lang="en">
       <body>
-      <Tabs defaultValue="dashboard">
-        <TabsList className="bg-black h-12 text-white">
-          <Link href={"/admin/dashboard"}>
-            <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
-          </Link>
+        <Tabs defaultValue="dashboard">
+          <TabsList className="bg-black h-12 text-white">
+            <Link href={"/admin/dashboard"}>
+              <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
+            </Link>
 
-          <Link href={"/admin/users"}>
-            <TabsTrigger value="users">Users</TabsTrigger>
-          </Link>
+            <Link href={"/admin/users"}>
+              <TabsTrigger value="users">Users</TabsTrigger>
+            </Link>
 
-          <Link href={"/admin/events"}>
-            <TabsTrigger value="events">events</TabsTrigger>
-          </Link>
+            <Link href={"/admin/events"}>
+              <TabsTrigger value="events">events</TabsTrigger>
+            </Link>
 
-          <Link href={"/admin/categories"}>
-            <TabsTrigger value="categories">Categories</TabsTrigger>
-          </Link>
+            <Link href={"/admin/categories"}>
+              <TabsTrigger value="categories">Categories</TabsTrigger>
+            </Link>
 
-          <Link href={"/admin/subcategories"}>
-            <TabsTrigger value="subcategories">Sub-Categories</TabsTrigger>
-          </Link>
-        </TabsList>
-        <TabsContent value="dashboard">{children} </TabsContent>
-        <TabsContent value="users">{children} </TabsContent>
-        <TabsContent value="events">{children} </TabsContent>
-        <TabsContent value="categories">{children} </TabsContent>
-        <TabsContent value="subcategories">{children} </TabsContent>
-      </Tabs>
+            <Link href={"/admin/subcategories"}>
+              <TabsTrigger value="subcategories">Sub-Categories</TabsTrigger>
+            </Link>
+          </TabsList>
+          <TabsContent value="dashboard">{children} </TabsContent>
+          <TabsContent value="users">{children} </TabsContent>
+          <TabsContent value="events">{children} </TabsContent>
+          <TabsContent value="categories">{children} </TabsContent>
+          <TabsContent value="subcategories">{children} </TabsContent>
+        </Tabs>
       </body>
-      </html>
-    )
+    </html>
+  );
 }
 
 export default Layout;
