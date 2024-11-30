aac01d248662f1230dcb006c3835565ee0e9e4a2
AbdulRehman Zia <abdulrehmanzia2021@gmail.com>
2024-11-30 14:25:25 +0500

    fixed searchParams bug

--- src/app/(admin)/admin/subcategories/page.js
+++ src/app/(admin)/admin/subcategories/page.js
@@ -1,11 +1,66 @@
-import SubCategoriesTable from "@/components/subcategoriesTable/subcategoriesTable";
+// "use  client"
+import { getCategories } from "@/action/categories";
+import { getSubCategories } from "@/action/subcategories";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table";
+import Image from "next/image";
+import DropDown from "../../../../components/dropDown/dropDown";
+import AddSubCategory from "../../../../components/addSubCategory/addSubCategory";
+  
 
-export default function Subcategories({ searchParams }) {
-  console.log("searchParams=>", searchParams);
+  export default async function SubCategoriesTable({ searchParams  }) {
+    console.log("Search Params in SubCategoriesTable =>", searchParams)
 
-  return (
-    <div className="min-h-screen mx-10 px-1">
-      <SubCategoriesTable />
-    </div>
-  );
-}
+    const subcategories = await getSubCategories()
+    const categories = await getCategories()    
+    // console.log("categories=>", categories);
+    
+    return (
+      <>
+      <div className="flex justify-between my-4">
+      <AddSubCategory />
+              <DropDown categories={categories}/>
+        </div>
+      <div className="min-h-screen mx-auto flex justify-center">
+        <Table >
+          <TableCaption>Sub-Categories</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead >Thumbnail</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Category</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {subcategories?.subcategories?.map((data) => (
+              <TableRow key={data.id}>
+                <TableCell>
+                  <Image 
+                  src={data.thumbnail}
+                  height={150}
+                  width={150}
+                  alt={data.thumbnail}
+                  />
+                  
+                  </TableCell>
+                <TableCell>{data.title}</TableCell>
+                <TableCell>{data.description}</TableCell>
+                <TableCell>{data.category?.title}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        
+      </div>
+      </>
+    );
+  }
+  
--- src/components/dropDown/dropDown.js
+++ src/components/dropDown/dropDown.js
@@ -10,12 +10,12 @@ import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export default function DropDown({ categories }) {
   const categoryList = categories?.categories;
-  console.log("Category List=>", categoryList);
+  // console.log("Category List=>", categoryList);
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
   const handleSelectCategory = (category) => {
-    console.log(category);
+    // console.log(category);
     const params = new URLSearchParams(searchParams);
     if (category) {
       params.set("category", category);
